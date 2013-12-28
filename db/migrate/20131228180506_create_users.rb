f621c9aeedd19b1f4502ee368d62313a39c0cd6a
Icelle <icelleku@gmail.com>
2013-12-28 13:25:42 -0500

    edit user and people model(syntax error) and migrate

--- db/migrate/20131228180506_create_users.rb
+++ db/migrate/20131228180506_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string, :first_name, index:true
-      t.string, :last_name, index:true
-      t.string, :user_name, index:true, unique:true
-      t.string, :email, index:true, unique:true
+      t.string :first_name, index: true
+      t.string :last_name, index: true
+      t.string :user_name, index: true
+      t.string :email, index: true
       t.string :password, index: true
 
       t.timestamps
