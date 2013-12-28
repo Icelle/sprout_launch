b5f6175f86ee1b0210a3b3355c17026a4234f407
Icelle <icelleku@gmail.com>
2013-12-28 14:44:39 -0500

    edit user model email_add to email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence:true
   validates :last_name, presence:true
   validates :user_name, presence:true
-  validates :email_add, presence:true, email:true
+  validates :email, presence:true, email:true
   validates :password, presence:true
 
   has_many :persons
