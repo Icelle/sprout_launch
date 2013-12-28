0efe5c554daa93a4c0dcee55eeae853e514a935d
Icelle <icelleku@gmail.com>
2013-12-28 15:42:53 -0500

    delete index:true from first_name, last_name and birthday in Person model

--- db/migrate/20131228180523_create_people.rb
+++ db/migrate/20131228180523_create_people.rb
@@ -1,10 +1,10 @@
 class CreatePeople < ActiveRecord::Migration
   def change
     create_table :people do |t|
-      t.string :first_name, index: true
-      t.string :last_name, index: true
+      t.string :first_name
+      t.string :last_name
       t.string :gender, index: true
-      t.date :birthday, index: true
+      t.date :birthday
       t.references :user, index: true
 
       t.timestamps
