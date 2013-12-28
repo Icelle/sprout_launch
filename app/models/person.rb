e01379dad9030338b2c196efd3adb302334400f3
Icelle <icelleku@gmail.com>
2013-12-28 15:23:58 -0500

    edit validation for gender and birthday for Person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,9 @@
 class Person < ActiveRecord::Base
   validates :first_name, presence:true
   validates :last_name, presence:true
-  validates :gender, presence:true
+  validates :gender, :inclusion => { :in => ['male', 'female'] }
   validates :birthday, presence:true
+  validates :birthday, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date}
 
   belongs_to :user,
     inverse_of: :persons
