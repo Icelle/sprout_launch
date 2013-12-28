4f7414e40d10bb379b6b8ec17fff4874b38282c8
Icelle <icelleku@gmail.com>
2013-12-28 11:51:22 -0500

    initial commit. set up gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sprout::Application.config.secret_key_base = 'f411c2452cbbcf21334ba7fbe2caaa9116ded20ae5f64790fa1e4839fc510bedb15c6255343dea9213b6b87b385b2a6d5286be2272c296e22ec9ae49896b7df4'
