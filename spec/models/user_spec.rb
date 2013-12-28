6f4f9db3a4c1fd3afc1941148ab3aea72e935cdb
Icelle <icelleku@gmail.com>
2013-12-28 14:45:45 -0500

    add testing for user and make test pass

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_valid(:first_name).when('Icelle')}
+  it {should_not have_valid(:first_name).when('', nil)}
+  it {should have_valid(:last_name).when('Ku')}
+  it {should_not have_valid(:last_name).when('', nil)}
+  it {should have_valid(:user_name).when('icelle')}
+  it {should_not have_valid(:user_name).when('', nil)}
+  it {should have_valid(:email).when('icelleku@gmail.com')}
+  it {should_not have_valid(:email).when('', nil)}
+  it {should have_valid(:password).when('abc123')}
+  it {should_not have_valid(:password).when('', nil)}
+
+  it { should have_many(:persons) }
 end
