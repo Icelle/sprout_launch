528ed633105a7a1bd4db3a84b79eeb00d5c534af
Icelle <icelleku@gmail.com>
2013-12-28 15:43:41 -0500

    write and make test pass for measurement model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,4 +1,7 @@
 class Measurement < ActiveRecord::Base
+  validates :height, presence:true, :numericality => {:greater_than => 0}
+  validates :weight, presence:true, :numericality => {:greater_than => 0}
+
   belongs_to :person,
     inverse_of: :measurements
 
--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -1,5 +1,8 @@
 require 'spec_helper'
 
 describe Measurement do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should have_valid(:height).when(275, 275.5)}
+  it {should_not have_valid(:height).when('', nil, 'a', 0, -1.0)}
+  it {should have_valid(:weight).when(50.5, 50)}
+  it {should_not have_valid(:weight).when('', nil, 'a', 0, -1.0)}
 end
