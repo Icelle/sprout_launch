e4d6981df1869b5135c081b0d03722295f3d1bf8
Icelle <icelleku@gmail.com>
2013-12-28 14:16:05 -0500

    create national average model

---
+++ db/migrate/20131228191052_create_national_averages.rb
@@ -0,0 +1,14 @@
+class CreateNationalAverages < ActiveRecord::Migration
+  def change
+    create_table :national_averages do |t|
+      t.string :gender
+      t.decimal :agemos
+      t.decimal :l
+      t.decimal :m
+      t.decimal :s
+      t.decimal :p50
+
+      t.timestamps
+    end
+  end
+end
