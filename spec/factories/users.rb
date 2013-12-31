372ab1c889e1cfaf313e84a0c199729487d79226
Icelle <icelleku@gmail.com>
2013-12-30 22:57:57 -0500

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
-    first_name ""
-    email ""
+    first_name "MyString"
+    last_name "Name"
+    user_name "userNAME"
+    sequence(:email) {|x| "test#{x}@email.com"}
     password "MyString"
   end
 end
