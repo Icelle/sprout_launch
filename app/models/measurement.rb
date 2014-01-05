86ab821482942374c71232b585f5629afd8b8a43
Icelle <icelleku@gmail.com>
2014-01-05 13:48:26 -0500

    change syntax for numericality validation on measurement

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -1,8 +1,7 @@
 class Measurement < ActiveRecord::Base
-  validates :height, presence:true, :numericality => {:greater_than => 0}
-  validates :weight, presence:true, :numericality => {:greater_than => 0}
+  validates_numericality_of :height, greater_than: 0, presence:true
+  validates_numericality_of :weight, greater_than: 0, presence:true
 
   belongs_to :person,
     inverse_of: :measurements
-
 end
