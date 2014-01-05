955da21a4cbfc2b0a330d8f1cb5f310c2f6ea626
Icelle <icelleku@gmail.com>
2014-01-05 13:49:07 -0500

    add unit test to person

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,20 +1,26 @@
 require 'spec_helper'
 
 describe Person do
-  it {should have_valid(:first_name).when('Icelle')}
-  it {should_not have_valid(:first_name).when('', nil)}
-  it {should have_valid(:last_name).when('Ku')}
-  it {should_not have_valid(:last_name).when('', nil)}
-  it {should have_valid(:gender).when('male')}
-  it {should have_valid(:gender).when('female')}
-  it {should_not have_valid(:gender).when('', nil)}
-  it {should_not have_valid(:gender).when('xaheqfldj')}
-  it {should_not have_valid(:birthday).when(0)}
-  it {should have_valid(:birthday).when(Date.parse('dec-23-1984'))}
-  it {should_not have_valid(:birthday).when(Date.parse('dec-23-2050'))}
-  it {should_not have_valid(:birthday).when('', nil)}
+  describe 'validations' do
+    let(:person) {FactoryGirl.build(:person)}
+    it 'is valid when it has all required and valid attributes' do
+      expect(person).to be_valid
+    end
 
-  it {should belong_to :user}
+    it {should have_valid(:first_name).when('Icelle')}
+    it {should_not have_valid(:first_name).when('', nil)}
+    it {should have_valid(:last_name).when('Ku')}
+    it {should_not have_valid(:last_name).when('', nil)}
+    it {should have_valid(:gender).when('male')}
+    it {should have_valid(:gender).when('female')}
+    it {should_not have_valid(:gender).when('', nil)}
+    it {should_not have_valid(:gender).when('xaheqfldj')}
+    it {should_not have_valid(:birthday).when(0)}
+    it {should have_valid(:birthday).when(Date.parse('dec-23-1984'))}
+    it {should_not have_valid(:birthday).when(Date.parse('dec-23-2050'))}
+    it {should_not have_valid(:birthday).when('', nil)}
 
-  it { should have_many(:measurements) }
+    it {should belong_to :user}
+    it {should have_many(:measurements) }
+  end
 end
