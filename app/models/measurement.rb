aae448f13642bb095c049795082813310f78c1a8
Icelle <icelleku@gmail.com>
2014-01-13 13:04:19 -0500

    calculate age using birthday and as_of date, ar search for national average match and percentile

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -7,5 +7,25 @@ class Measurement < ActiveRecord::Base
   belongs_to :person,
     inverse_of: :measurements
 
-  #def nationalaverage
+  def age_mos
+    ((self.as_of_dt - self.person.birthday)/30.41).to_f
+  end
+
+  def get_national_avg
+    match_1 = NationalAverage.where("agemos <= ? and gender = ? and stat_type = ?", self.age_mos, self.person.gender, self.measurement_type).order('agemos desc').first
+    match_2 = NationalAverage.where("agemos >= ? and gender = ? and stat_type = ?", self.age_mos, self.person.gender, self.measurement_type).order('agemos asc').first
+    if (self.age_mos - match_1.agemos).abs > (self.age_mos - match_2.agemos)
+      return match_2
+    else
+      return match_1
+    end
+  end
+
+  def percentile
+    natl_avg = self.get_national_avg
+    return natl_avg.percentile(self.value)
+  end
 end
+
+
+# date | weight/height | percentile
