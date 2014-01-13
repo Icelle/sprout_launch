6bbbd7d7d0303a0e67e46ef0bc1d399c9d7cb641
Icelle <icelleku@gmail.com>
2014-01-13 13:02:38 -0500

    write test for measurement model

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -8,11 +8,27 @@ describe Measurement do
       expect(measurement).to be_valid
     end
 
-    it {should have_valid(:height).when(275, 275.5)}
-    it {should_not have_valid(:height).when('', nil, 'a', 0, -1.0)}
-    it {should have_valid(:weight).when(50.5, 50)}
-    it {should_not have_valid(:weight).when('', nil, 'a', 0, -1.0)}
+    it {should have_valid(:measurement_type).when('weight', 'height')}
+    it {should_not have_valid(:measurement_type).when('', nil, 'abc', 1)}
+    it {should have_valid(:value).when(20.5, 100)}
+    it {should_not have_valid(:value).when('', nil, -1, -2.3, 0, "a")}
 
     it {should belong_to :person}
+  end
+
+  it 'should calculate age in months' do
+    p = FactoryGirl.create(:person_with_measurement)
+    m = p.measurements.first
+    expect(m.age_mos).to be_within(0.1).of(9)
+  end
+
+  it 'should find the corresponding National Average' do
+    p = FactoryGirl.create(:person_with_measurement, gender: "male")
+    m = p.measurements.first
+    # TODO: test both match conditions
+    expect((m.get_national_avg).agemos).to be_within(1).of(m.age_mos)
+
+    # get percentile
+    expect(m.percentile).to be_within(0.1).of(58)
   end
 end
