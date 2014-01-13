7f1879988459ca9c019780abd58189322b3062d9
Icelle <icelleku@gmail.com>
2014-01-12 19:11:03 -0500

    comment out everything on UsersController, don't need it for mandrill

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,45 @@
-class UsersController < ApplicationController
-  before_filter :user_is_current_user
-  def new
-    @user = User.new
-  end
+# class UsersController < ApplicationController
+#   before_filter :user_is_current_user
+#   def new
+#     @user = User.new
+#   end
 
-  def create
-    @user = User.new(params[:user])
-      respond_to do |format|
-        if @user.save
-          UserMailer.welcome_email(@user).deliver
-          format.html { redirect_to(@user, notice: 'User was successfully created.') }
-        else
-          format.html { render action: 'new' }
-        end
-      end
-    end
-  end
+#   def create
+#     @user = User.new(params[:user])
+#       respond_to do |format|
+#         if @user.save
+#           UserMailer.welcome_email(@user).deliver
+#           format.html { redirect_to(@user, notice: 'User was successfully created.') }
+#         else
+#           format.html { render action: 'new' }
+#         end
+#       end
+#     end
+#   end
 
-  def index
-    @user_is_current_user
-  end
+#   def index
+#     @user_is_current_user
+#   end
 
-  def failed
-    redirect_to root_path error: 'Failed to authenticate'
-  end
+#   def failed
+#     redirect_to root_path error: 'Failed to authenticate'
+#   end
 
-  def destroy
-    session[:user_id] = nil
-    redirect_to root_path, notice: 'Signed Out!'
-  end
+#   def destroy
+#     session[:user_id] = nil
+#     redirect_to root_path, notice: 'Signed Out!'
+#   end
 
 
-private
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :email)
-  end
+# private
+#   def user_params
+#     params.require(:user).permit(:first_name, :last_name, :email)
+#   end
 
-  def user_is_current_user
-    unless current_user.id == params[:user_id]
-      flash[:notice] = "Please sign in."
-      redirect_to root_path
-    end
-  end
-end
+#   def user_is_current_user
+#     unless current_user.id == params[:user_id]
+#       flash[:notice] = "Please sign in."
+#       redirect_to root_path
+#     end
+#   end
+# end
