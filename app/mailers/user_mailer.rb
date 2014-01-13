d3e9ba33e45d05d2d7e3be9d6ca975386a788b3a
Icelle <icelleku@gmail.com>
2014-01-12 19:31:11 -0500

    edit welcome_email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: 'icelleku@gmail.com'
+  default from: 'Sprout <sprout@sprout.icelle.com>'
 
   def welcome_email(user)
     @user = user
     @url  = 'http://sprout.icelle.com/login'
-    mail(to: @user.email, subject: 'Account Confirmation. Welcome to Sprout.')
+    mail(to: "#{@user.first_name} #{@user.last_name} <#{@user.email}>", subject: 'Welcome to Sprout!')
   end
 end
