9435a7456912086f5ed2a486474807aea3101063
Icelle <icelleku@gmail.com>
2014-01-13 13:02:56 -0500

    edit people factory

--- spec/factories/people.rb
+++ spec/factories/people.rb
@@ -1,5 +1,4 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
 require 'date'
 FactoryGirl.define do
   factory :person do
@@ -17,6 +16,20 @@ FactoryGirl.define do
   trait :female do
     name   "Baby Girl"
     gender "Female"
+  end
+
+  factory :person_with_measurements do
+    after(:create) do |p|
+       p.measurements << FactoryGirl.create(:measurement_height, {as_of_dt: Date.new(2012,1,4)})
+       p.measurements << FactoryGirl.create(:measurement_height, {as_of_dt: Date.new(2012,2,4)})
+       p.measurements << FactoryGirl.create(:measurement_height, {as_of_dt: Date.new(2012,3,15)})
+     end
+  end
+
+  factory :person_with_measurement do
+    after(:create) do |p|
+       p.measurements << FactoryGirl.create(:measurement_height, {as_of_dt: Date.new(2012,9,1)})
+     end
   end
 
   factory :person_male,   traits: [:male]
