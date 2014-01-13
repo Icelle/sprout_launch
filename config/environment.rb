e37a55c96e73ad3ac7c17bc2b73f2c8f07cbce62
Icelle <icelleku@gmail.com>
2014-01-12 19:14:08 -0500

    add mandrill api in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Sprout::Application.initialize!
+
+Sprout::Application.configure do
+  # access these via Rails.configuration, e.g. Rails.configuration.urls[:homepage]
+  config.urls = {
+    homepage: "http://sprout.icelle.com",
+    sign_in: "http://sprout.icelle.com/users/sign_in"
+  }
+
+  config.mandrill_mailer = Mandrill::API.new("QrSfN11kFNdZZbIRcDCUBQ")
+end
