ea767b233944e6d9db8fd2cee4c2419ca34ed7c1
Icelle <icelleku@gmail.com>
2014-01-15 13:24:51 -0500

    get x and y(measurement type) values for each person

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,4 +11,15 @@ class Person < ActiveRecord::Base
     inverse_of: :people
 
   has_many :measurements
+
+  # @param [String] type - type of measurement type to be returned for a given person
+  # @return [Array] collection of x,y coordinates for graphing. Converts times to seconds since unix epoch
+  def fetch_graph_data(type)
+    if !Measurement::MEASUREMENT_TYPES.include?(type)
+      raise "Invalid type #{type} specified. Must be one of: #{Measurement::MEASUREMENT_TYPES.to_s}"
+    else
+      # populate data by measurement type into {x: val, y: val}..[{x:898989, y:89}, {x:78787,y:90}, {x:78787,y:90}]
+      return self.measurements.where("measurement_type = ?", type).map{|measurement|{"x"=> measurement.as_of_dt.strftime('%s').to_i, "y" => measurement.value.to_f}}
+    end
+  end
 end
