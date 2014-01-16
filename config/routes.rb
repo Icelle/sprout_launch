16e9637db3113f951e534f861f7c053cfd727765
Icelle <icelleku@gmail.com>
2014-01-16 11:38:49 -0500

    add people route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Sprout::Application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  resources :people
   # You can have the root of your site routed with "root"
   root 'home#index'
 
