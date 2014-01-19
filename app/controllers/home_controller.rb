6c24f416aad051a5501f3fb62987d86ad65c75a3
Icelle <icelleku@gmail.com>
2014-01-19 14:07:56 -0500

    add skip_before_filter so everyone has access to home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,6 @@
 class HomeController < ApplicationController
+  skip_before_filter :authenticate_user!
+
   def index
-    gon.weight_data = fetch_data("weight")
-    gon.height_data = fetch_data("height")
-  end
-
-  def fetch_data(type)
-    data = []
-    people = current_user.people
-    colors = gen_colors(people.size)
-
-    people.each_with_index do |p, idx|
-      data.push({
-        color: colors[idx],
-        data: p.fetch_graph_data(type),
-        name: "#{p.first_name + " " + p.last_name}"
-        })
-    end
-    return data
   end
 end
