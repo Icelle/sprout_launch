d856cbf5e6a8b179db8b2303e2d446bb23436416
Icelle <icelleku@gmail.com>
2014-01-23 13:23:16 -0500

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,82 @@
+# config valid only for Capistrano 3.1
+lock '3.1.0'
+
+set :application, 'sproutly'
+set :repo_url, 'git@github.com:Icelle/sprout_launch.git'
+set :branch, 'master'
+set :deploy_to, "/var/www/sproutly.icelle.com"
+# Default branch is :master
+# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+# Default deploy_to directory is /var/www/my_app
+# set :deploy_to, '/var/www/my_app'
+
+# Default value for :scm is :git
+# set :scm, :git
+
+# Default value for :format is :pretty
+# set :format, :pretty
+
+# Default value for :log_level is :debug
+# set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, %w{config/database.yml}
+
+# Default value for linked_dirs is []
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+namespace :deploy do
+
+  desc "Check that we can access everything"
+  task :check_write_permissions do
+    on roles(:all) do |host|
+      if test("[ -w #{fetch(:deploy_to)} ]")
+        info "#{fetch(:deploy_to)} is writable on #{host}"
+      else
+        error "#{fetch(:deploy_to)} is not writable on #{host}"
+      end
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      execute :touch, release_path.join('tmp/restart.txt')
+
+      # execute "service nginx restart", :sudo => true
+    end
+  end
+
+
+  desc "Symlink shared configs and folders on each release."
+  task :symlink_shared do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "ln -nfs #{deploy_to}/shared/config/secrets.yml #{release_path}/config/secrets.yml"
+      execute "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    end
+  end
+
+  after :publishing, :restart
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+      #   execute :rake, 'cache:clear'
+      # end
+    end
+  end
+end
+
+after 'deploy:symlink:linked_files', 'deploy:symlink_shared'
